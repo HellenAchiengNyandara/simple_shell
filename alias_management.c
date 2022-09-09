b33baf7c9428c2e361e40d3e1538e0d296ad490f
Elizabeth-Onyango <lizz.onyango@gmail.com>
2022-09-09 05:45:10 -0700

    fourth commit

--- alias_management.c
+++ alias_management.c
@@ -1,6 +1,6 @@
 #include "shell.h"
 
-/*
+/**
 * print_alias - add, remove or show aliases
 * @data: struct for the program's data
 * @alias: name of the alias to be printed
@@ -8,11 +8,9 @@
 */
 
 int print_alias(data_of_program *data, char *alias)
-
 {
 	int i, j, alias_length;
 	char buffer[250] = {'\0'};
-
 
 	if (data->alias_list)
 	{
@@ -28,7 +26,6 @@ int print_alias(data_of_program *data, char *alias)
 					if (data->alias_list[i][j] == '=')
 						break;
 				}
-
 				buffer[j + 1] = '\0';
 				buffer_add(buffer, "'");
 				buffer_add(buffer, data->alias_list[i] + j + 1);
@@ -37,10 +34,8 @@ int print_alias(data_of_program *data, char *alias)
 			}
 		}
 	}
-
 	return (0);
 }
-
 
 
 /**
@@ -51,7 +46,6 @@ int print_alias(data_of_program *data, char *alias)
 */
 
 char *get_alias(data_of_program *data, char *name)
-
 {
 	int i, alias_length;
 
@@ -62,23 +56,16 @@ char *get_alias(data_of_program *data, char *name)
 	alias_length = str_length(name);
 
 	for (i = 0; data->alias_list[i]; i++)
-
 	{/* Iterates through the environ and check for coincidence of the varname */
 		if (str_compare(name, data->alias_list[i], alias_length) &&
-
-			data->alias_list[i][alias_length] == '='}
-
+			data->alias_list[i][alias_length] == '=')
 		{/* returns the value of the key NAME=  when find it */
 			return (data->alias_list[i] + alias_length + 1);
 		}
 	}
-
 	/* returns NULL if did not find it */
 	return (NULL);
-
-
 }
-
 
 /**
 * set_alias - add, or override alias
@@ -94,20 +81,18 @@ int set_alias(char *alias_string, data_of_program *data)
 
 	/* validate the arguments */
 	if (alias_string == NULL ||  data->alias_list == NULL)
-
 		return (1);
 
 	/* Iterates alias to find = char */
-
 	for (i = 0; alias_string[i]; i++)
 		if (alias_string[i] != '=')
 			buffer[i] = alias_string[i];
+
 		else
 		{/* search if the value of the alias is another alias */
 			temp = get_alias(data, alias_string + i + 1);
 			break;
 		}
-
 
 	/* Iterates through the alias list and check for coincidence of the varname */
 	for (j = 0; data->alias_list[j]; j++)
@@ -118,8 +103,6 @@ int set_alias(char *alias_string, data_of_program *data)
 			break;
 		}
 
-
-
 	/* add the alias */
 	if (temp)
 	{/* if the alias already exist */
@@ -127,7 +110,6 @@ int set_alias(char *alias_string, data_of_program *data)
 		buffer_add(buffer, temp);
 		data->alias_list[j] = str_duplicate(buffer);
 	}
-
 	else /* if the alias does not exist */
 		data->alias_list[j] = str_duplicate(alias_string);
 	return (0);
